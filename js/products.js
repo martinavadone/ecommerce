77af85e7c6a2816b94d3b73c8b1d52524f5d6ab6
martinavadone <martinavadone2001@icloud.com>
2021-08-15 17:24:19 -0300

    entrega 1

--- js/products.js
+++ js/products.js
@@ -2,5 +2,30 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(productos => {
+        prodData = productos.data;
+        let htmlContentToAppend = "";
+        for (producto of prodData) {
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ producto.name +`</h4>
+                            <small class="text-muted">` + producto.soldCount + ` vendidos</small>
+                        </div>
+                        <p class="mb-1">` + producto.description + `</p>
+                        <p class="mb-1 precio">` + producto.currency + ` ` + producto.cost + `</p>  
+                    </div>
+                </div>
+            </a>
+            `
+        }
+            document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+        });
+        });
 
-});
+    
